fd5dacaf1b357fe278770dfbb3fe8ecdc0e7df1d
Ben V. Brown <Ralim@Ralimtek.com>
2023-12-14 22:41:26 +1100

    Pivot to array2d
    .
    
    Refactor movements
    
    printer

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,157 +1,246 @@
+use array2d::Array2D;
 use jemallocator::Jemalloc;
 
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
 
-use std::fs::read_to_string;
+use std::{
+    fmt::{self},
+    fs::read_to_string,
+};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 
 enum GroundState {
     SquareRock,
     RoundRock,
     Dirt,
 }
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct VerticalStripe {
-    pub rock_set: Vec<GroundState>,
-}
-impl Default for VerticalStripe {
-    fn default() -> Self {
-        Self {
-            rock_set: Vec::with_capacity(100),
+impl fmt::Display for GroundState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            GroundState::SquareRock => write!(f, "#"),
+            GroundState::RoundRock => write!(f, "O"),
+            GroundState::Dirt => write!(f, "."),
         }
-    }
-}
-
-impl VerticalStripe {
-    pub fn ingest(&mut self, data: char) {
-        self.rock_set.push(match data {
-            '.' => GroundState::Dirt,
-            '#' => GroundState::SquareRock,
-            'O' => GroundState::RoundRock,
-            _ => panic!("Bad ground type"),
-        });
-    }
-    pub fn print(&self) {
-        for v in &self.rock_set {
-            match v {
-                GroundState::SquareRock => print!("#"),
-                GroundState::RoundRock => print!("O"),
-                GroundState::Dirt => print!("."),
-            }
-        }
-        println!();
-    }
-    pub fn move_round_rocks_up(&mut self) {
-        let mut dirt_start = 0xFFFF;
-
-        for i in 0..self.rock_set.len() {
-            match self.rock_set[i] {
-                GroundState::SquareRock => {
-                    //Cant be moved, so have to flush queued dirt then insert
-                    if dirt_start != 0xFFFF {
-                        for x in dirt_start..i {
-                            self.rock_set[x] = GroundState::Dirt
-                        }
-                        dirt_start = 0xFFFF;
-                    }
-                }
-                GroundState::RoundRock => {
-                    if dirt_start == 0xFFFF {
-                        //Cant move, leave in place
-                    } else {
-                        self.rock_set[dirt_start] = GroundState::RoundRock;
-                        dirt_start += 1;
-                    }
-                }
-                GroundState::Dirt => {
-                    if dirt_start == 0xFFFF {
-                        dirt_start = i;
-                    }
-                }
-            }
-        }
-        if dirt_start != 0xFFFF {
-            for x in dirt_start..self.rock_set.len() {
-                self.rock_set[x] = GroundState::Dirt
-            }
-        }
-    }
-    pub fn get_north_weight(&self) -> usize {
-        let mut sum = 0;
-        for (i, v) in self.rock_set.iter().enumerate() {
-            if *v == GroundState::RoundRock {
-                sum += self.rock_set.len() - i;
-            }
-        }
-        sum
     }
 }
 
 #[derive(Clone, PartialEq)]
 struct Grid {
-    stripes: Vec<VerticalStripe>,
+    grid: Array2D<GroundState>,
 }
 
 impl Grid {
     pub fn from_lines(lines: &[&str]) -> Self {
         //
-        let mut stripes = Vec::new();
+        let mut stripes: Vec<Vec<GroundState>> = Vec::new();
 
         for _ in 0..lines[0].len() {
-            stripes.push(VerticalStripe::default());
+            stripes.push(Vec::new());
         }
 
         for line in lines {
             for (i, c) in line.chars().enumerate() {
-                stripes[i].ingest(c);
+                stripes[i].push(match c {
+                    '.' => GroundState::Dirt,
+                    '#' => GroundState::SquareRock,
+                    'O' => GroundState::RoundRock,
+                    _ => panic!("Bad ground type"),
+                });
             }
         }
 
-        Self { stripes }
+        Self {
+            grid: Array2D::from_columns(&stripes).unwrap(),
+        }
     }
 
     pub fn print(&self) {
         println!("<------");
-        for m in self.stripes.iter().rev() {
-            m.print();
+        for row in self.grid.rows_iter() {
+            for c in row {
+                print!("{}", c);
+            }
+            println!();
         }
         println!("<------");
     }
-    pub fn slide_all_rocks_up(&mut self) {
-        self.stripes
-            .iter_mut()
-            .for_each(|x| x.move_round_rocks_up());
-    }
-    pub fn get_north_weight(&self) -> usize {
-        self.stripes.iter().map(|x| x.get_north_weight()).sum()
-    }
-    pub fn get_rotated_cw(&mut self) {
-        let mut stripes = Vec::with_capacity(self.stripes[0].rock_set.len());
+    pub fn slide_all_rocks_north(&mut self) {
+        //Walk down each column, and slide rocks up
+        for column in 0..self.grid.num_columns() {
+            let mut dirt_start = 0xFFFF;
 
-        for _ in 0..self.stripes[0].rock_set.len() {
-            stripes.push(VerticalStripe::default());
-        }
-
-        for stripe in &self.stripes {
-            for (i, v) in stripe.rock_set.iter().rev().enumerate() {
-                stripes[i].rock_set.push(*v);
+            for row in 0..self.grid.num_rows() {
+                match self.grid[(row, column)] {
+                    GroundState::SquareRock => {
+                        //Cant be moved, so have to flush queued dirt then insert
+                        if dirt_start != 0xFFFF {
+                            for x in dirt_start..row {
+                                self.grid[(x, column)] = GroundState::Dirt;
+                            }
+                            dirt_start = 0xFFFF;
+                        }
+                    }
+                    GroundState::RoundRock => {
+                        if dirt_start == 0xFFFF {
+                            //Cant move, leave in place
+                        } else {
+                            self.grid[(dirt_start, column)] = GroundState::RoundRock;
+                            dirt_start += 1;
+                        }
+                    }
+                    GroundState::Dirt => {
+                        if dirt_start == 0xFFFF {
+                            dirt_start = row;
+                        }
+                    }
+                }
+            }
+            if dirt_start != 0xFFFF {
+                for x in dirt_start..self.grid.num_rows() {
+                    self.grid[(x, column)] = GroundState::Dirt;
+                }
             }
         }
-        self.stripes = stripes;
+    }
+
+    pub fn slide_all_rocks_east(&mut self) {
+        //Walk across the row, sliding right
+        for row in 0..self.grid.num_rows() {
+            let mut dirt_start = 0xFFFF;
+
+            for column in (0..self.grid.num_columns()).rev() {
+                match self.grid[(row, column)] {
+                    GroundState::SquareRock => {
+                        //Cant be moved, so have to flush queued dirt then insert
+                        if dirt_start != 0xFFFF {
+                            for x in column..dirt_start {
+                                self.grid[(row, x + 1)] = GroundState::Dirt;
+                            }
+                            dirt_start = 0xFFFF;
+                        }
+                    }
+                    GroundState::RoundRock => {
+                        if dirt_start == 0xFFFF {
+                            //Cant move, leave in place
+                        } else {
+                            self.grid[(row, dirt_start)] = GroundState::RoundRock;
+                            dirt_start -= 1;
+                        }
+                    }
+                    GroundState::Dirt => {
+                        if dirt_start == 0xFFFF {
+                            dirt_start = column;
+                        }
+                    }
+                }
+            }
+            if dirt_start != 0xFFFF {
+                for x in 0..dirt_start + 1 {
+                    self.grid[(row, x)] = GroundState::Dirt;
+                }
+            }
+        }
+    }
+
+    pub fn slide_all_rocks_west(&mut self) {
+        //Walk across the row, sliding left
+        for row in 0..self.grid.num_rows() {
+            let mut dirt_start = 0xFFFF;
+
+            for column in 0..self.grid.num_columns() {
+                match self.grid[(row, column)] {
+                    GroundState::SquareRock => {
+                        //Cant be moved, so have to flush queued dirt then insert
+                        if dirt_start != 0xFFFF {
+                            for x in dirt_start..column {
+                                self.grid[(row, x)] = GroundState::Dirt;
+                            }
+                            dirt_start = 0xFFFF;
+                        }
+                    }
+                    GroundState::RoundRock => {
+                        if dirt_start == 0xFFFF {
+                            //Cant move, leave in place
+                        } else {
+                            self.grid[(row, dirt_start)] = GroundState::RoundRock;
+                            dirt_start += 1;
+                        }
+                    }
+                    GroundState::Dirt => {
+                        if dirt_start == 0xFFFF {
+                            dirt_start = column;
+                        }
+                    }
+                }
+            }
+            if dirt_start != 0xFFFF {
+                for x in dirt_start..self.grid.num_columns() {
+                    self.grid[(row, x)] = GroundState::Dirt;
+                }
+            }
+        }
+    }
+    pub fn slide_all_rocks_south(&mut self) {
+        //Walk down each column, and slide rocks down
+        for column in 0..self.grid.num_columns() {
+            let mut dirt_start = 0xFFFF;
+
+            for row in (0..self.grid.num_rows()).rev() {
+                match self.grid[(row, column)] {
+                    GroundState::SquareRock => {
+                        //Cant be moved, so have to flush queued dirt then insert
+                        if dirt_start != 0xFFFF {
+                            for x in row..dirt_start {
+                                self.grid[(x + 1, column)] = GroundState::Dirt;
+                            }
+                            dirt_start = 0xFFFF;
+                        }
+                    }
+                    GroundState::RoundRock => {
+                        if dirt_start == 0xFFFF {
+                            //Cant move, leave in place
+                        } else {
+                            self.grid[(dirt_start, column)] = GroundState::RoundRock;
+                            dirt_start -= 1;
+                        }
+                    }
+                    GroundState::Dirt => {
+                        if dirt_start == 0xFFFF {
+                            dirt_start = row;
+                        }
+                    }
+                }
+            }
+            if dirt_start != 0xFFFF {
+                for x in (0..dirt_start + 1).rev() {
+                    self.grid[(x, column)] = GroundState::Dirt;
+                }
+            }
+        }
+    }
+
+    pub fn get_north_weight(&self) -> usize {
+        let mut sum = 0;
+        for (x, row) in self.grid.rows_iter().enumerate() {
+            for v in row {
+                if *v == GroundState::RoundRock {
+                    sum += self.grid.num_rows() - x;
+                }
+            }
+        }
+        sum
     }
 }
 fn rotate_one_iter(grid: &mut Grid) {
-    grid.slide_all_rocks_up();
-    grid.get_rotated_cw();
-    grid.slide_all_rocks_up();
-    grid.get_rotated_cw();
-    grid.slide_all_rocks_up();
-    grid.get_rotated_cw();
-    grid.slide_all_rocks_up();
-    grid.get_rotated_cw()
+    grid.slide_all_rocks_north();
+
+    grid.slide_all_rocks_west();
+
+    grid.slide_all_rocks_south();
+
+    grid.slide_all_rocks_east();
 }
 fn read_file(filename: &str) -> usize {
     let file_contents = read_to_string(filename).unwrap();
